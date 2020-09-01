b0c4693f6798084185d7c629644be481657ad6de
Anthony Protano <anthony@clevyr.com>
2020-09-01 14:58:42 -0500

    Image field now works for non pagebuilder pages

--- src/resources/views/vendor/backpack/crud/fields/image.blade.php
+++ src/resources/views/vendor/backpack/crud/fields/image.blade.php
@@ -44,16 +44,16 @@
 @endphp
 
 @include('crud::fields.inc.wrapper_start')
-    <div>
-        <label>{!! $field['label'] !!}</label>
-        @include('crud::fields.inc.translatable_icon')
+<div>
+    <label>{!! $field['label'] !!}</label>
+    @include('crud::fields.inc.translatable_icon')
+</div>
+<!-- Wrap the image or canvas element with a block element (container) -->
+<div class="row">
+    <div class="col-sm-6" data-handle="previewArea" style="margin-bottom: 20px;">
+        <img data-handle="mainImage" src="">
     </div>
-    <!-- Wrap the image or canvas element with a block element (container) -->
-    <div class="row">
-        <div class="col-sm-6" data-handle="previewArea" style="margin-bottom: 20px;">
-            <img data-handle="mainImage" src="">
-        </div>
-        @if(isset($field['crop']) && $field['crop'])
+    @if(isset($field['crop']) && $field['crop'])
         <div class="col-sm-3" data-handle="previewArea">
             <div class="docs-preview clearfix">
                 <div class="img-preview preview-lg">
@@ -61,28 +61,32 @@
                 </div>
             </div>
         </div>
-        @endif
-    </div>
-    <div class="btn-group">
-        <div class="btn btn-light btn-sm btn-file">
-            {{ trans('backpack::crud.choose_file') }} <input type="file" accept="image/*" data-handle="uploadImage"  @include('crud::fields.inc.attributes')>
+    @endif
+</div>
+<div class="btn-group">
+    <div class="btn btn-light btn-sm btn-file">
+        {{ trans('backpack::crud.choose_file') }} <input type="file" accept="image/*" data-handle="uploadImage"  @include('crud::fields.inc.attributes')>
+        @if (isset($sections))
             <input type="hidden" data-handle="hiddenImage" name="" value="{{ $storage }}">
             <input type="hidden" data-handle="hiddenImage" name="{{ $field['name'] }}" value="{{ $value }}" />
-        </div>
-        @if(isset($field['crop']) && $field['crop'])
+        @else
+            <input type="hidden" data-handle="hiddenImage" name="{{ $field['name'] }}" value="{{ $storage ?? $value }}" />
+        @endif
+    </div>
+    @if(isset($field['crop']) && $field['crop'])
         <button class="btn btn-light btn-sm" data-handle="rotateLeft" type="button" style="display: none;"><i class="la la-rotate-left"></i></button>
         <button class="btn btn-light btn-sm" data-handle="rotateRight" type="button" style="display: none;"><i class="la la-rotate-right"></i></button>
         <button class="btn btn-light btn-sm" data-handle="zoomIn" type="button" style="display: none;"><i class="la la-search-plus"></i></button>
         <button class="btn btn-light btn-sm" data-handle="zoomOut" type="button" style="display: none;"><i class="la la-search-minus"></i></button>
         <button class="btn btn-light btn-sm" data-handle="reset" type="button" style="display: none;"><i class="la la-times"></i></button>
-        @endif
-        <button class="btn btn-light btn-sm" data-handle="remove" type="button"><i class="la la-trash"></i></button>
-    </div>
-
-    {{-- HINT --}}
-    @if (isset($field['hint']))
-        <p class="help-block">{!! $field['hint'] !!}</p>
     @endif
+    <button class="btn btn-light btn-sm" data-handle="remove" type="button"><i class="la la-trash"></i></button>
+</div>
+
+{{-- HINT --}}
+@if (isset($field['hint']))
+    <p class="help-block">{!! $field['hint'] !!}</p>
+@endif
 @include('crud::fields.inc.wrapper_end')
 
 
@@ -147,127 +151,127 @@
         <script src="{{ asset('packages/jquery-cropper/dist/jquery-cropper.min.js') }}"></script>
         <script>
             function bpFieldInitCropperImageElement(element) {
-                    // Find DOM elements under this form-group element
-                    var $mainImage = element.find('[data-handle=mainImage]');
-                    var $uploadImage = element.find("[data-handle=uploadImage]");
-                    var $hiddenImage = element.find("[data-handle=hiddenImage]");
-                    var $rotateLeft = element.find("[data-handle=rotateLeft]");
-                    var $rotateRight = element.find("[data-handle=rotateRight]");
-                    var $zoomIn = element.find("[data-handle=zoomIn]");
-                    var $zoomOut = element.find("[data-handle=zoomOut]");
-                    var $reset = element.find("[data-handle=reset]");
-                    var $remove = element.find("[data-handle=remove]");
-                    var $previews = element.find("[data-handle=previewArea]");
-                    // Options either global for all image type fields, or use 'data-*' elements for options passed in via the CRUD controller
-                    var options = {
-                        viewMode: 2,
-                        checkOrientation: false,
-                        autoCropArea: 1,
-                        responsive: true,
-                        preview : element.find('.img-preview'),
-                        aspectRatio : element.attr('data-aspectRatio')
-                    };
-                    var crop = element.attr('data-crop');
+                // Find DOM elements under this form-group element
+                var $mainImage = element.find('[data-handle=mainImage]');
+                var $uploadImage = element.find("[data-handle=uploadImage]");
+                var $hiddenImage = element.find("[data-handle=hiddenImage]");
+                var $rotateLeft = element.find("[data-handle=rotateLeft]");
+                var $rotateRight = element.find("[data-handle=rotateRight]");
+                var $zoomIn = element.find("[data-handle=zoomIn]");
+                var $zoomOut = element.find("[data-handle=zoomOut]");
+                var $reset = element.find("[data-handle=reset]");
+                var $remove = element.find("[data-handle=remove]");
+                var $previews = element.find("[data-handle=previewArea]");
+                // Options either global for all image type fields, or use 'data-*' elements for options passed in via the CRUD controller
+                var options = {
+                    viewMode: 2,
+                    checkOrientation: false,
+                    autoCropArea: 1,
+                    responsive: true,
+                    preview : element.find('.img-preview'),
+                    aspectRatio : element.attr('data-aspectRatio')
+                };
+                var crop = element.attr('data-crop');
 
-                    // Hide 'Remove' button if there is no image saved
-                    if (!$hiddenImage.val()){
-                        $previews.hide();
+                // Hide 'Remove' button if there is no image saved
+                if (!$hiddenImage.val()){
+                    $previews.hide();
+                    $remove.hide();
+                }
+                // Make the main image show the image in the hidden input
+                $mainImage.attr('src', $hiddenImage.val());
+
+
+                // Only initialize cropper plugin if crop is set to true
+                if(crop){
+
+                    $remove.click(function() {
+                        $mainImage.cropper("destroy");
+                        $mainImage.attr('src','');
+                        $hiddenImage.val('');
+                        $rotateLeft.hide();
+                        $rotateRight.hide();
+                        $zoomIn.hide();
+                        $zoomOut.hide();
+                        $reset.hide();
                         $remove.hide();
-                    }
-                    // Make the main image show the image in the hidden input
-                    $mainImage.attr('src', $hiddenImage.val());
-
-
-                    // Only initialize cropper plugin if crop is set to true
-                    if(crop){
-
-                        $remove.click(function() {
-                            $mainImage.cropper("destroy");
-                            $mainImage.attr('src','');
-                            $hiddenImage.val('');
-                            $rotateLeft.hide();
-                            $rotateRight.hide();
-                            $zoomIn.hide();
-                            $zoomOut.hide();
-                            $reset.hide();
-                            $remove.hide();
-                            $previews.hide();
-                        });
-                    } else {
-
-                        $remove.click(function() {
-                            $mainImage.attr('src','');
-                            $hiddenImage.val('');
-                            $remove.hide();
-                            $previews.hide();
-                        });
-                    }
-
-                    $uploadImage.change(function() {
-                        var fileReader = new FileReader(),
-                                files = this.files,
-                                file;
-
-                        if (!files.length) {
-                            return;
-                        }
-                        file = files[0];
-
-                        const maxImageSize = {{ $max_image_size_in_bytes }};
-                        if(maxImageSize > 0 && file.size > maxImageSize) {
-
-                            alert(`Please pick an image smaller than ${maxImageSize} bytes.`);
-                        } else if (/^image\/\w+$/.test(file.type)) {
-
-                            fileReader.readAsDataURL(file);
-                            fileReader.onload = function () {
-
-                                $uploadImage.val("");
-                                $previews.show();
-                                if(crop){
-                                    $mainImage.cropper(options).cropper("reset", true).cropper("replace", this.result);
-                                    // Override form submit to copy canvas to hidden input before submitting
-                                    // update the hidden input after selecting a new item or cropping
-                                    $mainImage.on('ready cropstart cropend', function() {
-                                        var imageURL = $mainImage.cropper('getCroppedCanvas').toDataURL(file.type);
-                                        $hiddenImage.val(imageURL);
-                                        return true;
-                                    });
-                                    $rotateLeft.click(function() {
-                                        $mainImage.cropper("rotate", 90);
-                                    });
-                                    $rotateRight.click(function() {
-                                        $mainImage.cropper("rotate", -90);
-                                    });
-                                    $zoomIn.click(function() {
-                                        $mainImage.cropper("zoom", 0.1);
-                                    });
-                                    $zoomOut.click(function() {
-                                        $mainImage.cropper("zoom", -0.1);
-                                    });
-                                    $reset.click(function() {
-                                        $mainImage.cropper("reset");
-                                    });
-                                    $rotateLeft.show();
-                                    $rotateRight.show();
-                                    $zoomIn.show();
-                                    $zoomOut.show();
-                                    $reset.show();
-                                    $remove.show();
-
-                                } else {
-                                    $mainImage.attr('src',this.result);
-                                    $hiddenImage.val(this.result);
-                                    $remove.show();
-                                }
-                            };
-                        } else {
-                            new Noty({
-                                type: "error",
-                                text: "<strong>Please choose an image file</strong><br>The file you've chosen does not look like an image."
-                            }).show();
-                        }
+                        $previews.hide();
                     });
+                } else {
+
+                    $remove.click(function() {
+                        $mainImage.attr('src','');
+                        $hiddenImage.val('');
+                        $remove.hide();
+                        $previews.hide();
+                    });
+                }
+
+                $uploadImage.change(function() {
+                    var fileReader = new FileReader(),
+                        files = this.files,
+                        file;
+
+                    if (!files.length) {
+                        return;
+                    }
+                    file = files[0];
+
+                    const maxImageSize = {{ $max_image_size_in_bytes }};
+                    if(maxImageSize > 0 && file.size > maxImageSize) {
+
+                        alert(`Please pick an image smaller than ${maxImageSize} bytes.`);
+                    } else if (/^image\/\w+$/.test(file.type)) {
+
+                        fileReader.readAsDataURL(file);
+                        fileReader.onload = function () {
+
+                            $uploadImage.val("");
+                            $previews.show();
+                            if(crop){
+                                $mainImage.cropper(options).cropper("reset", true).cropper("replace", this.result);
+                                // Override form submit to copy canvas to hidden input before submitting
+                                // update the hidden input after selecting a new item or cropping
+                                $mainImage.on('ready cropstart cropend', function() {
+                                    var imageURL = $mainImage.cropper('getCroppedCanvas').toDataURL(file.type);
+                                    $hiddenImage.val(imageURL);
+                                    return true;
+                                });
+                                $rotateLeft.click(function() {
+                                    $mainImage.cropper("rotate", 90);
+                                });
+                                $rotateRight.click(function() {
+                                    $mainImage.cropper("rotate", -90);
+                                });
+                                $zoomIn.click(function() {
+                                    $mainImage.cropper("zoom", 0.1);
+                                });
+                                $zoomOut.click(function() {
+                                    $mainImage.cropper("zoom", -0.1);
+                                });
+                                $reset.click(function() {
+                                    $mainImage.cropper("reset");
+                                });
+                                $rotateLeft.show();
+                                $rotateRight.show();
+                                $zoomIn.show();
+                                $zoomOut.show();
+                                $reset.show();
+                                $remove.show();
+
+                            } else {
+                                $mainImage.attr('src',this.result);
+                                $hiddenImage.val(this.result);
+                                $remove.show();
+                            }
+                        };
+                    } else {
+                        new Noty({
+                            type: "error",
+                            text: "<strong>Please choose an image file</strong><br>The file you've chosen does not look like an image."
+                        }).show();
+                    }
+                });
             }
         </script>
 
